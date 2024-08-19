8a639e504284e7320ac76651746f262b91fedc50
Nuffles <nuffles0123626@gmail.com>
2024-08-19 19:32:27 +0800

    adding protection route

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,38 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './Login.jsx';
 import Admin from './Admin.jsx';
 import Employee from './Employee.jsx';
 import './index.css';
+import ProtectedRoute from './components/ProtectedRoute.jsx';
 
 const root = createRoot(document.getElementById('root'));
+
+function Logout(){
+  localStorage.clear()
+  return  <Navigate to="/"/>
+}
+
+function RegisterAndLogout(){
+  localStorage.clear()
+  return <Register />
+}
 
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/admin" element={<Admin />} />
-        <Route path="/employee" element={<Employee />} />
+        <Route path="/admin" element={<ProtectedRoute>
+            <Admin />
+          </ProtectedRoute>
+        } />
+        <Route path="/employee" element={
+          <ProtectedRoute>
+            <Employee />
+          </ProtectedRoute>
+        } />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
