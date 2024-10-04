a955ffa1180964019a0d9f04878c26f5abde66ef
Nuffles <nuffles0123626@gmail.com>
2024-10-04 23:36:38 +0800

    backend rollback

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -33,11 +33,9 @@ DEBUG = True
 ALLOWED_HOSTS = ["*"]
 
 REST_FRAMEWORK = {
-    "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
-    ),
-    "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.IsAuthenticated",
+    'DEFAULT_AUTHENTICATION_CLASSES': [],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
     ],
 }
 
